481cacd307181531b2860643ca727b8c4fd11031
stephen_boxerman@taylor.edu <sboxerma@cse21705.cs.local>
2019-09-13 13:52:22 -0400

    Moved randome play over to a diferent file for easier implementation of AIs

--- bid-whist-rules.py
+++ bid-whist-rules.py
@@ -4,10 +4,9 @@
 # Author2:  Asher Gingerich
 # --------------------------------------
 
-import math
 import random
-import numpy as np
 import os
+import Player
 
 players = [[],[],[],[]]
 GOAL_POINTS = 1
@@ -123,48 +122,51 @@ def setup(dealer):
         if bids != ['p', 'p', 'p', 'p']:
             return bids
 
-
+# a function for finding the winning bid
 def getWinningBid(bids, dealer):
 
-    bidValues = []
+    bidValues = [] #variable to hold the values of the bids that have been made (see POSSIBLE_BIDS for the bid values)
     for bid in bids:
         bidValues.append(POSSIBLE_BIDS.get(bid))
     winningBid = bidValues.index(max(bidValues))
+    # set leading player to be the player that own the bid
     leadingPlayer = (winningBid + 1 + dealer) % 4
+
+    # set the winning criteria for winning a trick
     if bidValues[winningBid] % 2 == 0:
-        trump = False
         restriction = input("High or low? (asc or desc) -> ")
         while restriction not in ['asc', 'desc']:
             print('Invalid.  Must enter asc or desc.')
             restriction = input("High or low? (asc or desc) -> ")
     else:
-        trump = True
         restriction = input("What's trump? (h, d, s, c) -> ")
         while restriction not in ['h', 'd', 's', 'c']:
             print('Invalid.  Must enter h, d, s, or c.')
             restriction = input("What's trump? (h, d, s, c) -> ")
+
     return leadingPlayer, restriction, winningBid
 
-
+# function for a player to play a card
 def play_card(player):
     card = ''
     while True:
         card = input('player' + str(player) + ' play a card. -> ')
+        #logic to determin if the player has the card in hand
         if card not in players[player]:
             print("You do not have that card in hand. Please play a legal card.")
         else:
             return card
 
-
+# helper function for determining if a suit is in a players hand
 def has_suit(player, suit):
     return player_suits[player][suit] != 0
 
-
+# helper function to remove a card from a players hand
 def remove_card(player, card):
     card_index = players[player].index(card)
     players[player].pop(card_index)
 
-
+#a function to find the number of of cards played that are of a given suit an thier values
 def findSuit(played, suit, default = -1):
     card_values = []
     for card in played:
@@ -176,7 +178,7 @@ def findSuit(played, suit, default = -1):
 
     return card_values
 
-
+# a function for finding the winning card of a trick 
 def findWinningCard(playedCards, restriction, leadSuit):
 
     if restriction == "desc":
@@ -192,24 +194,24 @@ def findWinningCard(playedCards, restriction, leadSuit):
             winningCard = max(values)
 
         winningCard = max(values)
-
+    #return the index of the winning card to determin what player that card belonged to
     return values.index(winningCard)
 
-
+# a function to exicute the game play for a single hand
 def play(leadingPlayer, restriction):
     for i in range(13):
-        team1 = [0,2]
-        team2 = [1,3]
         tricks = [0, 0]
         playedCards = []
 
-        card = play_card(leadingPlayer)
+        card = play_card(leadingPlayer) # have the leading player play a card
         playedCards.append(card)
-        leadSuit = get_card_suit(card)
-        current_player = next_player(leadingPlayer)
+        leadSuit = get_card_suit(card) #set the suit that all other players must play if posable
+        current_player = next_player(leadingPlayer) #set current player to the curent players left
         for i in range(3):
-            card = play_card(current_player)
-            while get_card_suit(card) != get_card_suit(playedCards[0]):
+            card = play_card(current_player) #have a player play a card
+
+	    # logic to determin if player has played a legal card
+            while get_card_suit(card) != get_card_suit(playedCards[0]): 
                 if has_suit(current_player, get_card_suit(playedCards[0])):
                     print('You must follow suit.')
                     card = play_card(current_player)
@@ -222,10 +224,10 @@ def play(leadingPlayer, restriction):
 
         winningCard = findWinningCard(playedCards, restriction, leadSuit)
 
-        winningPlayer = (leadingPlayer + winningCard) % 4
+        winningPlayer = (leadingPlayer + winningCard) % 4  #find who won the hand
         print("Player " + str(winningPlayer) + " won the trick.\n")
 
-        tricks[winningPlayer % 2] += 1
+        tricks[winningPlayer % 2] += 1 #acumulate tricks for winning team
         leadingPlayer = winningPlayer
 
     return tricks
@@ -262,25 +264,31 @@ def random_play(leadingPlayer, restriction):
 
     return tricks
 
-
+# a function that calculates the sore for both teams after a hand is finished
 def calcScores(tricks, winningBid, leadingPlayer, restriction):
 
     leadingTeam = leadingPlayer % 2 # leadingTeam[0] = player0, player2 - Asher Gingerich
-    nonLeadingTeam = (leadingTeam+1) % 2
-    print(nonLeadingTeam)
-
+    nonLeadingTeam = (leadingTeam+1) % 2 # nonLeadingTeam = player1, player3
+    
+    # set the number of points that the team who won the bid must make to gain points, else the lose that many points
     trickGoal = int(winningBid[0])
-
+    
+    # takes into acount the book rule
     trickPotential = [tricks[0]-6,tricks[1]-6]
 
     points = [0, 0]
 
+    #point calculation
     if trickPotential[leadingTeam] > trickGoal:
         points[leadingTeam] += trickPotential[leadingTeam]
+	#NOTE: if the leadingTeam made thier bid, it is imposable for the other team to have made points
     else:
         points[leadingTeam] -= trickGoal
+	#NOTE: If leadingTeam did NOT make thier bid, it is posable that the other team made points
         if trickPotential[nonLeadingTeam] > 0:
             points[nonLeadingTeam] += trickPotential[leadingTeam]
+
+    # double points gained/lost if no trump
     if restriction in ["asc", "desc"]:
         points[leadingTeam] *= 2
     print(points)
@@ -288,25 +296,34 @@ def calcScores(tricks, winningBid, leadingPlayer, restriction):
 
 
 def main(debug = False):
+    #remove any files made by previous runs
     remove_files()
+
+    #dealer is default as 0
     dealer = 0
     team1Score = 0
     team2Score = 0
     while True:
 
-        bids = setup(dealer)
-        leadingPlayer, restriction, winningIndex = getWinningBid(bids, dealer)
+        bids = setup(dealer)# go through pre-play setup
+	
+        leadingPlayer, restriction, winningIndex = getWinningBid(bids, dealer)# determine leading player and index of winning bid
+
         winningBid = bids[winningIndex]
+	# use randome players for easier testing (also considered first "AI")
         if debug:
             tricks = random_play(leadingPlayer, restriction)
         else:
             tricks = play(leadingPlayer, restriction)
-        points = calcScores(tricks, winningBid, leadingPlayer, restriction)
+
+        points = calcScores(tricks, winningBid, leadingPlayer, restriction)# get points won by teams
+
         team1Score += points[0]
         team2Score += points[1]
 
         print("Team 1: " + str(team1Score) + "\nTeam2: " + str(team2Score))
-
+	
+	# logic for determining winner of the game
         if team1Score >= GOAL_POINTS or team2Score <= -GOAL_POINTS:
             print("Team 1 Won!!!")
             break
@@ -315,6 +332,6 @@ def main(debug = False):
             print("Team 2 Won!!!")
             break
 
-    remove_files()
+    remove_files() #remove files at end of game
 
 main()
